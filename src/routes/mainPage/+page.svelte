8bd7242d47d5b1369676d9eabad2d81c7768188d
Elgius <114131653+Elgius@users.noreply.github.com>
2023-04-07 00:22:17 +0500

    minor update

---
+++ src/routes/mainPage/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+
+</script>
+  
+  <section>
+    <div class="container">
+      <div class="flex justify-center items-center">
+        <h1 class='text-3xl'>Honesty</h1>
+      </div>
+    </div>
+  
+    <div class="flex justify-center items-center">
+      <img src="" alt="placeholder" class="my-10" />
+    </div>
+  
+    <div class="flex justify-center items-center flex-col">
+      <input label="Username" placeholder="Username" class="my-2" />
+      <button class="my-2">Submit</button>
+    </div>
+  </section>
+  
+  <style>
+    .container {
+      max-width: 640px;
+      margin: 0 auto;
+    }
+  </style>
