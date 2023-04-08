5c2e0adfe305ecbbf3f5dd8fe3d32b212f1d820c
Elgius <114131653+Elgius@users.noreply.github.com>
2023-04-08 12:23:45 +0500

    sample

---
+++ src/routes/account/+page.svelte
@@ -0,0 +1,46 @@
+<script>
+</script>
+
+<section>
+	<div class="container">
+		<div class="flex items-center justify-center my-10">
+			<div class="card w-96 bg-base-100 shadow-xl">
+				<div class="card-body">
+					<h2 class="card-title">Card title!</h2>
+					<p>If a dog chews shoes whose shoes does he choose?</p>
+					<div class="card-actions justify-end">
+						<button class="btn btn-primary">Buy Now</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="container">
+		<div class="flex items-center justify-center my-10">
+			<div class="card w-96 bg-base-100 shadow-xl">
+				<div class="card-body">
+					<h2 class="card-title">Card title!</h2>
+					<p>If a dog chews shoes whose shoes does he choose?</p>
+					<div class="card-actions justify-end">
+						<button class="btn btn-primary">Buy Now</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="container">
+		<div class="flex items-center justify-center my-10">
+			<div class="card w-96 bg-base-100 shadow-xl">
+				<div class="card-body">
+					<h2 class="card-title">Card title!</h2>
+					<p>If a dog chews shoes whose shoes does he choose?</p>
+					<div class="card-actions justify-end">
+						<button class="btn btn-primary">Buy Now</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
