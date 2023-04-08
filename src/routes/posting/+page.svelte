64d1f714eb5bc64e207eda64de7a7a7add7fc300
Elgius <114131653+Elgius@users.noreply.github.com>
2023-04-08 14:50:44 +0500

    stable build

---
+++ src/routes/posting/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+</script>
+
+<section>
+	<div class="text-center">
+
+        <h1 class='text-3xl font-bold'>Honesty</h1>
+	</div>
+
+
+    <div class='border-black'>
+        <textarea name="cool" id="" cols="30" rows="10"></textarea>
+    </div>
+
+    <div>
+        <!-- star rating thing here, probab;y first JS script thing i gotta write this project -->
+    </div>
+
+
+    <div>
+        <input type="checkbox">
+        <p>Anon</p>
+    </div>
+
+
+    <div class="btn btn-secondary">
+        <button>Submit</button>
+    </div>
+
+</section>
