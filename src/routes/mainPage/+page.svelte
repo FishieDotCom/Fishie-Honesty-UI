11f96c0821fea35ae4f8d833367e6f1c18d6e689
Elgius <114131653+Elgius@users.noreply.github.com>
2023-04-08 12:06:21 +0500

    Done login page skeleton

--- src/routes/mainPage/+page.svelte
+++ src/routes/mainPage/+page.svelte
@@ -1,27 +1,47 @@
 <script>
-
 </script>
-  
-  <section>
-    <div class="container">
-      <div class="flex justify-center items-center">
-        <h1 class='text-3xl'>Honesty</h1>
-      </div>
-    </div>
-  
-    <div class="flex justify-center items-center">
-      <img src="" alt="placeholder" class="my-10" />
-    </div>
-  
-    <div class="flex justify-center items-center flex-col">
-      <input label="Username" placeholder="Username" class="my-2" />
-      <button class="my-2">Submit</button>
-    </div>
-  </section>
-  
-  <style>
-    .container {
-      max-width: 640px;
-      margin: 0 auto;
-    }
-  </style>
+
+<section>
+	<div class="container">
+		<div class="flex justify-center items-center flex-col my-10">
+			<h1 class="text-5xl font-bold">Honesty</h1>
+			<p class="text-sm fint-bold">by fishie</p>
+		</div>
+	</div>
+
+	<div class="flex justify-center items-center mt-30 mb-20">
+		<div class="avatar">
+			<div class="w-60 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+				<img src="https://images.unsplash.com/photo-1578507065211-1c4e99a5fd24?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="sample image" />
+			</div>
+		</div>
+	</div>
+
+  <div class="flex items-center justify-center">
+  <div class="form-control w-full max-w-xs">
+    <label class="label">
+      <span class="label-text">What is your Username?</span>
+    </label>
+    <input type="text" placeholder="@UserName" class="input input-bordered w-full max-w-xs" />
+    <label class="label">
+      <span class="label-text-alt text-green-500 text-xl">Username is available</span>
+    </label>
+  </div>
+</div>
+
+<div class='container'>
+  <div class="flex items-center justify-center mt-10">
+  <button class="btn btn-secondary">
+    Submit
+  </button>
+</div>
+</div>
+
+</section>
+
+<style>
+	.container {
+		max-width: 640px;
+		margin: 0 auto;
+	}
+</style>
