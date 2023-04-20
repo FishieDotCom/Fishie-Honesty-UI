bc229c088614cee2b083d7a548855f4ae974f8c1
fISHIE <83373559+WhoIsFishie@users.noreply.github.com>
2023-04-21 01:55:53 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
-import dns from 'dns'
-
-dns.setDefaultResultOrder('verbatim')
 
 
 export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
-	},
-  	server: {
-    port: 5183,
-    strictPort: true
-  	}
+	}
 });
 
