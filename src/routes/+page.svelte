9f68864e266891634f73605bb94ed0f04f6f5ed4
fISHIE <83373559+WhoIsFishie@users.noreply.github.com>
2023-04-21 01:19:56 +0500

    hard code test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -37,7 +37,7 @@
 				requestAccess={true}
 				on:auth={async (auth) => {
 					console.log(auth.detail);
-					// const login = await fetch(apiDomain + '/api/Auth/logIn', {
+					// const login = await fetch('https://honestyapi.sar.sh' + '/api/Auth/logIn', {
 					// 	method: 'POST',
 					// 	headers: {
 					// 		'Content-Type': 'application/json'
