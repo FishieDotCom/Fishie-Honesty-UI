84736a3aff6b024dd508e4f33c6f9a60d3d7e58b
fISHIE <83373559+WhoIsFishie@users.noreply.github.com>
2023-04-21 01:44:03 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,11 @@ export default defineConfig({
 	},
   	server: {
     		host: true,
-    		port: 80    
+    		port: 80,
+		strictPort: true,
+		hmr: {
+            		port: 443,
+        	},
   	}
 });
 
