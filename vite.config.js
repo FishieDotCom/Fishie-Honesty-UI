54a1a9f2ef3e3a91f2bd7ab36d9c97ca0000fced
fISHIE <83373559+WhoIsFishie@users.noreply.github.com>
2023-04-21 01:57:50 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,15 @@ export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+	},
+	server: {
+    port: 5173,
+    https: true,
+    hmr: {
+        host: "honestyui.sar.sh/",
+        port: 5173,
+        protocol: "wss",
+    }
+},
 });
 
